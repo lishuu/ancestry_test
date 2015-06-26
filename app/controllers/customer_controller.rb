f359eebab6a338f404ebc3cddf6a94e98cfba037
lishu <subsky@gmail.com>
2015-06-26 11:35:04 +0800

    点击片区、小区、楼宇节点表格中显示相应片区、小区、楼宇的用户信息

--- app/controllers/customer_controller.rb
+++ app/controllers/customer_controller.rb
@@ -5,15 +5,16 @@ class CustomerController < ApplicationController
     cc = params[:cc]
     cb = params[:cb]
 
-    puts "片区ID:#{cd}"
-    puts "小区ID:#{cc}"
-    puts "楼宇ID:#{cb}"
+   customer_filters = {}
+   customer_filters[:district_id] = cd if cd 
+   customer_filters[:community_id] = cc if cc
+   customer_filters[:building_id] = cb if cb
 
-
+   puts customer_filters
 
   	respond_to do |format|
   	  format.html
-      format.json { render json: CustomerDatatable.new(view_context, {community_id: cc, district_id: cd} ) }
+      format.json { render json: CustomerDatatable.new( view_context, customer_filters ) }
   	end
   end
 
